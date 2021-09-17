049b4559fe72b6931c710fd14ac100542a557c3b
Dennis Mullen <dkmullen@gmail.com>
2021-09-16 20:42:18 -0400

    Hook up to sqlite db

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///all_books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class BookForm(FlaskForm):
@@ -13,11 +17,21 @@ class BookForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
-all_books = []
+# Model the book
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+# Create the DB
+db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
@@ -25,14 +39,35 @@ def home():
 def add():
     if request.method == "POST":
         print(request.form)
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template("add.html")
+
+
+@app.route("/edit-rating", methods=["GET", "POST"])
+def edit_rating():
+    book_id = request.args.get('id')
+    book_to_update = Book.query.get(book_id)
+    if request.method == "POST":
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", book=book_to_update)
+
+
+@app.route('/delete-book')
+def delete_book():
+    book_id = request.args.get('id')
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
